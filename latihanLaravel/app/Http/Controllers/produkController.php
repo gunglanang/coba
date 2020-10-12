4b1e27f10a7955ae58cd22615898e7ba0f97a019
unknown <gunglanang14@gmail.com>
2020-10-12 09:44:10 +0800

    latihan mengunakan route dengan controller

---
+++ latihanLaravel/app/Http/Controllers/produkController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class produkController extends Controller
+{
+    //
+	public function index(){
+		$isi = "ini merupakan isi dari propery index dalam controller prudukcontroller";
+		return $isi;
+		}
+	
+}
--- latihanLaravel/routes/web.php
+++ latihanLaravel/routes/web.php
@@ -1,16 +1,34 @@
-<?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+	<?php
+	
+	use Illuminate\Support\Facades\Route;
+	use App\Http\Controllers\produkController;
+	/*
+	|--------------------------------------------------------------------------
+	| Web Routes
+	|--------------------------------------------------------------------------
+	|
+	| Here is where you can register web routes for your application. These
+	| routes are loaded by the RouteServiceProvider within a group which
+	| contains the "web" middleware group. Now create something great!
+	|
+	*/
+	
+	Route::get('/', function () {
+		return view('welcome');
+	});
+	
+	Route::get('/aboutme', function(){
+		echo "Ini adalah percobaan menampilkan Page";
+	})->name("tentang");
+	
+	Route::get('/show/{id?}', function($id=1){
+		echo "Parameter ID: " . $id;
+	})->where('id','[0-9]+');
+	
+	Route::get('/utama',function(){
+		echo "Ini Page Utama";
+		echo "<br>";
+		echo "<a href='".route('tentang')."'>About</a>";
+	});
+	
+	Route::get('/produk',[produkController::class ,'index']);
