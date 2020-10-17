6ca4d106ab328a8bae74b2b32c6743acf7d8603d
unknown <gunglanang14@gmail.com>
2020-10-17 21:38:34 +0800

    pertemuan 3 view

--- latihanLaravel/app/Http/Controllers/produkController.php
+++ latihanLaravel/app/Http/Controllers/produkController.php
@@ -6,10 +6,14 @@ use Illuminate\Http\Request;
 
 class produkController extends Controller
 {
-    //
-	public function index(){
-		$isi = "ini merupakan isi dari propery index dalam controller prudukcontroller";
-		return $isi;
-		}
-	
+    //Property untuk Index
+    public function index(){
+        $produk = "sandal jc 289";
+        return view('produk/index');
+    }
+    //Property Showproduk
+    public function showproduk(){
+        $produk = ["sandal jc 289","buku y 788", "kuwe 787","tas 888"];
+        return view('produk/showproduk', compact('produk'));
+    }
 }
--- latihanLaravel/routes/web.php
+++ latihanLaravel/routes/web.php
@@ -1,34 +1,39 @@
-	<?php
-	
-	use Illuminate\Support\Facades\Route;
-	use App\Http\Controllers\produkController;
-	/*
-	|--------------------------------------------------------------------------
-	| Web Routes
-	|--------------------------------------------------------------------------
-	|
-	| Here is where you can register web routes for your application. These
-	| routes are loaded by the RouteServiceProvider within a group which
-	| contains the "web" middleware group. Now create something great!
-	|
-	*/
-	
-	Route::get('/', function () {
-		return view('welcome');
-	});
-	
-	Route::get('/aboutme', function(){
-		echo "Ini adalah percobaan menampilkan Page";
-	})->name("tentang");
-	
-	Route::get('/show/{id?}', function($id=1){
-		echo "Parameter ID: " . $id;
-	})->where('id','[0-9]+');
-	
-	Route::get('/utama',function(){
-		echo "Ini Page Utama";
-		echo "<br>";
-		echo "<a href='".route('tentang')."'>About</a>";
-	});
-	
-	Route::get('/produk',[produkController::class ,'index']);
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\productcontroller;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/aboutme', function(){
+    echo "Ini adalah percobaan menampilkan Page";
+})->name("tentang");
+
+Route::get('/show/{id?}', function($id=1){
+    echo "Parameter ID: " . $id;
+})->where('id','[0-9]+');
+
+Route::get('/utama',function(){
+    echo "Ini Page Utama";
+    echo "<br>";
+    echo "<a href='".route('tentang')."'>About</a>";
+});
+
+Route::get('/produk',[produkController::class ,'index']);
+
+route::get('/latihanView01',function(){
+    return view("latihan01");
+});
+
+route::get('/produk/showproduk',[produkController::class,'showproduk']);
